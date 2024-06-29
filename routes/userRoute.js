f7a1285eb51efb63a0d132ebcd9ab14a64a6da93
RohitRaj766 <rohitraj2k04@gmail.com>
2024-06-29 15:23:39 +0530

    .

---
+++ routes/userRoute.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+const Quiz = require('../model/quiz')
+
+router.get('/quizzesList', authMiddleware, async (req, res) => {
+    try {
+        const quizzes = await Quiz.find({}, 'title date time');
+        res.status(200).json(quizzes);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+router.get('/quiz/:id', authMiddleware, async (req, res) => {
+    try {
+        const quiz = await Quiz.findById(req.params.id);
+        if (!quiz) {
+            return res.status(404).json({ message: "Quiz not found" });
+        }
+        res.status(200).json(quiz);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const express = require('express');
 const db = require('./config/db')
 const bodyParser = require('body-parser');
 const adminRouter = require('./routes/adminRoute');
-const TotalQuiz = require('./model/quiz');
+const userRouter = require('./routes/userRoute');
 
 const app = express();
 const PORT = 5000;
-app.use(bodyParser.json())//req body
+app.use(bodyParser.json());
 
-app.use('/admin',adminRouter)
+app.use('/admin',adminRouter);
+app.use('/user',userRouter);
 
 
 app.listen(PORT || 3000,()=>{
-    console.log(`Server Runing at ${PORT}`)
+    console.log(`Server Runing at ${PORT}`);
 })
