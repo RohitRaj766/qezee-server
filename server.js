32c49e8b0e98c3a7f63b5b3b2e7c86f9bfce6727
RohitRaj766 <rohitraj2k04@gmail.com>
2024-06-30 16:43:37 +0530

    .

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ require('dotenv').config()
 
 const db = require('./config/db')
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const adminRouter = require('./routes/adminRoute');
 const userRouter = require('./routes/userRoute');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/admin',adminRouter);
 app.use('/user',userRouter);
