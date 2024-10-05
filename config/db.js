b49cf304d907edab99412d24456bfb74f7f55629
RohitRaj766 <rohitraj2k04@gmail.com>
2024-10-05 15:45:21 +0530

    .

--- config/db.js
+++ config/db.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URL_PRODUCTION, {
+// mongoose.connect(process.env.MONGO_URL_PRODUCTION, {
+//     // useNewUrlParser: true,
+//     // useUnifiedTopology: true
+// }).then(() => console.log('connected to mongoDB server')).catch((err) => console.error('connection to mongoDB error', err));
+
+
+mongoose.connect(process.env.MONGO_URL, {
     // useNewUrlParser: true,
     // useUnifiedTopology: true
 }).then(() => console.log('connected to mongoDB server')).catch((err) => console.error('connection to mongoDB error', err));
 
 
-// mongoose.connect(process.env.MONGO_URL_DEVELOPMENT, {
-//     // useNewUrlParser: true,
-//     // useUnifiedTopology: true
-// }).then(() => console.log('connected to mongoDB server')).catch((err) => console.error('connection to mongoDB error', err));
 
 const db = mongoose.connection;
 
